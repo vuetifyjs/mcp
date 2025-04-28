3d19049922a90657f35b16f324c78fc06fbdc08f
Andrei Elkin <andreyyolkin@gmail.com>
2025-04-29 00:12:43 +0200

    feat: add interactive prompts for IDE selection [wip]
    We need this to patch global mcp.json files

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,28 +1,39 @@
+#!/usr/bin/env node
 import { intro } from '../cli/intro.js'
+import { confirm, multiselect } from '@clack/prompts'
+import type { DetectedIDE } from './detect-ide.js'
+import { detectIDEs } from './detect-ide.js'
+
+const ides = detectIDEs()
 
 intro('config')
 
-// TODO: use @clack/prompts and ask user to add to global settings for selected IDEs:
-/**
-  Example:
-import { confirm, multiselect } from '@clack/prompts';
+if (ides.length === 0) {
+  process.exit(0)
+}
 
 const shouldAddGlobally = await confirm({
   message: 'Add to global settings?',
-});
-  const idesToInstall = []
-  if (confirm !== true) { // might be symbol or boolean
-    process.exit(0)
-  }
-  if (confirm && ides.length > 1) {
-    // ask to multiselect IDEs to add to global settings
-    const idesToInstall = await multiselect({
-      message: 'Select IDEs to add to global settings',
-      options: ides.map(ide => ({ value: ide.ide, label: ide.ide })),
-    });
-  } else  {
-    // push firstIDE to idesToInstall
-  }
+})
 
-  Now we have paths, we have ides, need to read settings.json as jsonc and add to it
- */
+let idesToInstall: DetectedIDE[] | symbol = []
+if (shouldAddGlobally !== true) { // might be symbol or boolean
+  process.exit(0)
+}
+
+if (shouldAddGlobally && ides.length > 1) {
+  idesToInstall = await multiselect({
+    message: 'Select IDEs to add to global settings',
+    options: ides.map(ide => ({ value: ide, label: ide.brand })),
+  })
+}
+
+if (shouldAddGlobally && ides.length === 1) {
+  idesToInstall = ides
+}
+
+if (Array.isArray(idesToInstall) && idesToInstall.length > 0) {
+  console.log('idesToInstall', idesToInstall)
+} else {
+  console.log('No IDEs selected')
+}
