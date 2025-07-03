92aaecc58376167b14dced891d845ab4bea09e73
Henry <haviles04@gmail.com>
2025-07-03 14:27:14 -0400

    feat(tools/one): added conditional tool for creating a bin

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -7,8 +7,13 @@ import type { McpServer } from '@modelcontextprotocol/sdk/server/mcp.js'
 
 import { registerApiTools } from '#tools/api'
 import { registerDocumentationTools } from '#tools/documentation'
+import { registerOneTools } from "./one.js";
 
 export async function registerTools (server: McpServer) {
   await registerApiTools(server)
   await registerDocumentationTools(server)
+  const apiKey = process.env.VUETIFY_API_KEY || ''
+  if(apiKey.length) {
+    await registerOneTools(server)
+  }
 }
